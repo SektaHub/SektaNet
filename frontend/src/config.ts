2bbeb3dc52701e634053ee949244352e6ba1f0c1
Borjan Peovski <51805234+PeovskiB@users.noreply.github.com>
2024-03-05 16:20:17 +0100

    Update config.ts

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -7,5 +7,5 @@
 
 
 // PROD
-export const API_URL: string = 'http://backend:8080/api';
+export const API_URL: string = 'http://192.168.0.68:8080/api';
 //export const BASE_URL: string = 'https://localhost:3000';
