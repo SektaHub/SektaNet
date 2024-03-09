a21e8dfe8dc3d979e99fa13796bfd8c785b27963
Borjan Peovski <51805234+PeovskiB@users.noreply.github.com>
2024-03-09 14:37:07 +0100

    [CHANGE] port to 8081

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -7,5 +7,5 @@
 
 
 // PROD
-export const API_URL: string = 'http://192.168.0.68:8080/api';
+export const API_URL: string = 'http://api.cicki.gratis/api';
 //export const BASE_URL: string = 'https://localhost:3000';
