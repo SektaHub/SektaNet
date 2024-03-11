0920ab3894003d3f391429f97a108408f8d7b5ea
Borjan Peovski <51805234+PeovskiB@users.noreply.github.com>
2024-03-11 02:08:25 +0100

    Update config.ts

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -7,5 +7,5 @@
 
 
 // PROD
-export const API_URL: string = 'http://api.cicki.gratis/api';
+export const API_URL: string = 'https://api.cicki.gratis/api';
 //export const BASE_URL: string = 'https://localhost:3000';
