dd23a0e431d0d7f9fea81d4b1b646729f1bff3b7
Borjan Peovski <51805234+PeovskiB@users.noreply.github.com>
2024-12-17 19:21:10 +0100

    fixed up docker

--- backend/Program.cs
+++ backend/Program.cs
@@ -94,8 +94,8 @@ builder.Services.AddScoped<GenericFileService>();
 builder.Services.AddScoped<LongVideoService>();
 builder.Services.AddScoped<AudioService>();
 builder.Services.AddScoped<DiscordService>();
-builder.Services.AddScoped<AIService>();
 builder.Services.AddScoped<BlogpostService>();
+builder.Services.AddScoped<AIService>();
 
 //builder.Services.AddScoped<MongoDBService>();
 
